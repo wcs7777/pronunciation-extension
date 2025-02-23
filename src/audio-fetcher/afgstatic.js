edd4bd4e1b65c86bc9fb1bb5c8b1b04c63f0e4e8
wcs7777 <wcs777git@gmail.com>
2025-02-23 06:49:04 -0300

    add 20200429 date to gstatic audio candidates urls

--- src/audio-fetcher/afgstatic.js
+++ src/audio-fetcher/afgstatic.js
@@ -58,18 +58,25 @@ export default class AFGstatic {
 	 * @returns {Promise<Blob>}
 	 */
 	fetch(input) {
-		const base = "https://ssl.gstatic.com/dictionary/static/sounds/20220808";
+		const base = "https://ssl.gstatic.com/dictionary/static/sounds";
 		const fileBegin = input.replaceAll("'", "_");
-		const candidates = [
-			"--1_us_1.mp3",
-			"--_us_1.mp3",
-			"--_us_1_rr.mp3",
-			"--_us_2.mp3",
-			"--_us_2_rr.mp3",
-			"--_us_3.mp3",
-			"--_us_3_rr.mp3",
-			"_--1_us_1.mp3",
-		].map(fileEnd => `${base}/${fileBegin}${fileEnd}`);
+		/**
+		 * @type {string[]}
+		 */
+		let candidates = [];
+		for (const date of ["20200429", "20220808"]) {
+			const candidatesDate = [
+				"--1_us_1.mp3",
+				"--_us_1.mp3",
+				"--_us_1_rr.mp3",
+				"--_us_2.mp3",
+				"--_us_2_rr.mp3",
+				"--_us_3.mp3",
+				"--_us_3_rr.mp3",
+				"_--1_us_1.mp3",
+			].map(fileEnd => `${base}/${date}/${fileBegin}${fileEnd}`);
+			candidates = candidates.concat(candidatesDate);
+		}
 		return Promise.any(candidates.map(url => url2blob(url)));
 	}
 
